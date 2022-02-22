b5a15b50049e8f5f9606bec40285413791baf94b
Wesley Ferreira <wesleyferreirasb@gmail.com>
2022-02-22 11:08:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var (fileOrigin, fileDestiny) = (@"arquivo_origem.txt", @"arquivo_destino.txt");
+            var linesOrigin = System.IO.File.ReadAllLines(fileOrigin);
+            var linesDestiny = new string[linesOrigin.Length];
+            for (int i = 0; i < linesOrigin.Length; i++)
+            {
+                linesDestiny[i] = linesOrigin[i].StartsWith("B00") ?
+                    linesDestiny[i] = @$"{linesOrigin[i].Substring(0, 11)}
+                                          01022022
+                                         {linesOrigin[i].Substring(19, 6)}
+                                         01022022
+                                         {linesOrigin[i].Substring(33, linesOrigin[i].Length - 33)}" :
+                    linesDestiny[i] = linesOrigin[i];
+            }
+            System.IO.File.WriteAllLines(fileDestiny, linesDestiny);
+        }
+        catch (System.Exception)
+        {
+            throw;
+        };
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Leitor de arquivo txt#
+lê um arquivo txt e salva em um novo com os campos alterados
